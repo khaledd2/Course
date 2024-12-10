fe638e95aab33f67f0b766d22e8ebf1975d58d9f
KHALED BANI <khaledbasem183@gmail.com>
2024-12-10 18:37:20 +0200

    Category service

---
+++ Course.Core/Interfaces/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Course.Shared;
+using Course.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<BaseResponse<IEnumerable<CategoryDTO>>> GetAllCategoriesAsync();
+        public Task<BaseResponse<CategoryDTO>> GetCategoryByIdAsync(int id); 
+        public Task<BaseResponse<CategoryDTO>> UpdateCategoryAsync(CategoryDTO category);
+        public Task<BaseResponse<CategoryDTO>> CreateCategoryAsync(CategoryDTO category);
+        public Task<BaseResponse<CategoryDTO>> RemoveCategoryAsync(int categoryId);
+
+    }
+}
---
+++ Course.Core/Services/CategoryService.cs
@@ -0,0 +1,139 @@
+﻿using Course.BLL.Interfaces;
+using Course.Shared;
+using Course.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.DAL;
+using Course.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Course.Shared.Constants;
+namespace Course.BLL.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<BaseResponse<CategoryDTO>> CreateCategoryAsync(CategoryDTO category)
+        {
+            try
+            {
+                int maxCategoryId = 0;
+
+                if (_db.Categories.Any()) 
+                    maxCategoryId = await _db.Categories.MaxAsync(c => c.Id);
+
+                var entity = new Category
+                {
+                    Id = maxCategoryId + 1,
+                    Name = category.Name
+                };
+
+                _db.Categories.Add(entity);
+                await _db.SaveChangesAsync();
+
+                category.Id = entity.Id;
+                return new BaseResponse<CategoryDTO>(category, Messages.AddedSuccessfully);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<CategoryDTO>(null, Messages.Error, new List<string> { ex.Message }, false);
+            }
+        }
+
+        public async Task<BaseResponse<CategoryDTO>> UpdateCategoryAsync(CategoryDTO category)
+        {
+            try
+            {
+                var entity = await _db.Categories.FindAsync(category.Id);
+                if (entity == null)
+                {
+                    return new BaseResponse<CategoryDTO>(null, Messages.NotFound, null, false);
+                }
+
+                entity.Name = category.Name;
+
+                _db.Categories.Update(entity);
+                await _db.SaveChangesAsync();
+
+                return new BaseResponse<CategoryDTO>(category, Messages.UpdatedSuccessfully);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<CategoryDTO>(null, Messages.Error, new List<string> { ex.Message }, false);
+            }
+        }
+
+        public async Task<BaseResponse<IEnumerable<CategoryDTO>>> GetAllCategoriesAsync()
+        {
+            try
+            {
+                var categories = await _db.Categories
+                    .Select(c => new CategoryDTO
+                    {
+                        Id = c.Id,
+                        Name = c.Name
+                    }).ToListAsync();
+
+                return new BaseResponse<IEnumerable<CategoryDTO>>(categories, Messages.RetrievedSuccessfully);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<IEnumerable<CategoryDTO>>(null, Messages.Error, new List<string> { ex.Message }, false);
+            }
+        }
+
+        public async Task<BaseResponse<CategoryDTO>> GetCategoryByIdAsync(int id)
+        {
+            try
+            {
+                var entity = await _db.Categories.FindAsync(id);
+                if (entity == null)
+                {
+                    return new BaseResponse<CategoryDTO>(null, Messages.NotFound, null, false);
+                }
+
+                var category = new CategoryDTO
+                {
+                    Id = entity.Id,
+                    Name = entity.Name
+                };
+
+                return new BaseResponse<CategoryDTO>(category, Messages.RetrievedSuccessfully);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<CategoryDTO>(null, Messages.Error, new List<string> { ex.Message }, false);
+            }
+        }
+
+        public async Task<BaseResponse<CategoryDTO>> RemoveCategoryAsync(int categoryId)
+        {
+            try
+            {
+                var entity = await _db.Categories.FindAsync(categoryId);
+                if (entity == null)
+                {
+                    return new BaseResponse<CategoryDTO>(null, Messages.NotFound, null, false);
+                }
+
+                _db.Categories.Remove(entity);
+                await _db.SaveChangesAsync();
+
+                return new BaseResponse<CategoryDTO>(null, Messages.RemovedSuccessfully);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<CategoryDTO>(null, Messages.Error, new List<string> { ex.Message }, false);
+            }
+        }
+    }
+
+}

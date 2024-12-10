fe92522ad9c2b2ddea12e1eb1b1c45ae623d1068
KHALED BANI <khaledbasem183@gmail.com>
2024-12-10 18:37:43 +0200

    Useful classes

---
+++ Course.Shared/BaseResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Course.Shared
+{
+    public class BaseResponse<T>(T? data, string message, List<string> errors = null, bool success = true)
+    {
+        public bool Success { get; set; } = success;
+        public string Message { get; set; } = message;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<string>? Errors { get; set; } = errors;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public T? Data { get; set; } = data;
+    }
+
+}
---
+++ Course.Shared/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Shared.Constants
+{
+    public static class Messages
+    {
+        public static string Error => "حدث خطأ غير متوقع";
+        public static string AddedSuccessfully => "تمت الإضافة بنجاح";
+        public static string UpdatedSuccessfully => "تم التحديث بنجاح";
+        public static string RemovedSuccessfully => "تم الحذف بنجاح";
+        public static string NotFound => "لم يتم العثور على العنصر";
+        public static string RetrievedSuccessfully => "تم جلب البيانات بنجاح";
+        public static string ReferenceError => "لم يتم الحذف، تأكد من عدم وجود ارتباطات بهذا العنصر";
+
+    }
+
+}

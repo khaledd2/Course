7ee7c8231c824aa76cbf94bf04c4cbe239e48c97
KHALED BANI <khaledbasem183@gmail.com>
2024-12-12 19:42:24 +0200

    Create RandomStringGenerator.cs

---
+++ Course.Shared/Helpers/RandomStringGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Shared.Helpers
+{
+    public static class RandomStringGenerator
+    {
+        private static readonly Random _random = new Random();
+        private const string _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        public static string GenerateRandomString(int length = 8) 
+        {
+            return new string(Enumerable.Repeat(_chars, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}

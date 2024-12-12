7f1572c24a085980fd2113b676d71add4af9b5a0
KHALED BANI <khaledbasem183@gmail.com>
2024-12-12 19:42:30 +0200

    Create Pagination.cs

---
+++ Course.Shared/Records/Pagination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Shared.Records
+{
+    public record Pagination
+    {
+        public int PageIndex { get; set; } = 1;
+        [Range(1, 20)]
+        public int PageSize { get; set; } = 10;
+        public string Search {  get; set; } = string.Empty;
+    }
+}

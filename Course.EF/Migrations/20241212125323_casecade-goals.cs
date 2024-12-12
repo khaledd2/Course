8f93358afcd3075ee52991cea60d4751c05d91af
KHALED BANI <khaledbasem183@gmail.com>
2024-12-12 19:42:15 +0200

    New migration

---
+++ Course.EF/Migrations/20241212125323_casecade-goals.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Course.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class casecadegoals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goal_Course",
+                table: "Goal");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goal_Course",
+                table: "Goal",
+                column: "Course_Id",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goal_Course",
+                table: "Goal");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goal_Course",
+                table: "Goal",
+                column: "Course_Id",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

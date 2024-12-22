a3ddda62f7560c1f2e35654f363c74633f46c505
KHALED BANI <khaledbasem183@gmail.com>
2024-12-22 15:51:28 +0200

    refactor pagination class

---
+++ Course.Shared/Interfaces/IPagination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Shared.Interfaces
+{
+    public interface IPagination
+    {
+        int PageNumber { get; set; }
+        int PageSize { get; set; }
+        string Search { get; set; } 
+        int Skip();
+    }
+}
--- Course.Shared/Records/Pagination.cs
+++ Course.Shared/Records/Pagination.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Course.Shared.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,13 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 namespace Course.Shared.Records
 {
-    public record Pagination
+    public record Pagination : IPagination
     {
         public int PageNumber { get; set; } = 1;
         [Range(1, 20)]
         public int PageSize { get; set; } = 10;
         public string Search {  get; set; } = string.Empty;
-
         public int Skip() => (PageNumber - 1) * PageSize;
     }
 }

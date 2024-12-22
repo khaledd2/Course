94ea932c2c436d171d9fb5f023d4f6bb7a093775
KHALED BANI <khaledbasem183@gmail.com>
2024-12-22 16:29:57 +0200

    Update Pagination.cs

--- Course.Shared/Records/Pagination.cs
+++ Course.Shared/Records/Pagination.cs
@@ -10,8 +10,9 @@ namespace Course.Shared.Records
 {
     public record Pagination : IPagination
     {
+        [Range(1, 300, ErrorMessage = "تخطيت الحد المسموح به من الصفحات")]
         public int PageNumber { get; set; } = 1;
-        [Range(1, 20)]
+        [Range(1, 20, ErrorMessage = "تخطيت الحد المسموح به لحجم الصفحة")]
         public int PageSize { get; set; } = 10;
         public string Search {  get; set; } = string.Empty;
         public int Skip() => (PageNumber - 1) * PageSize;

7ff05c909c7eadd5b7854d150c40f5d73f75117e
KHALED BANI <khaledbasem183@gmail.com>
2024-12-26 16:37:38 +0200

    Answer service

---
+++ Course.Core/Interfaces/IAnswerService.cs
@@ -0,0 +1,15 @@
+﻿using Course.Shared;
+using Course.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.BLL.Interfaces
+{
+    public interface IAnswerService
+    {
+        Task<BaseResponse<List<AnswerDTO>>> GetAllAnswersByQuestionIdAsync(int questionId);
+    }
+}
---
+++ Course.Core/Services/AnswersService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Course.BLL.Interfaces;
+using Course.DAL;
+using Course.DAL.Models;
+using Course.Shared;
+using Course.Shared.Constants;
+using Course.Shared.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.BLL.Services
+{
+    public class AnswersService : IAnswerService
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+        public AnswersService(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        
+        public async Task<BaseResponse<List<AnswerDTO>>> GetAllAnswersByQuestionIdAsync(int questionId)
+        {
+            try
+            {
+                var items = await _db.Answers.Where(q => q.QuestionId == questionId).ToListAsync();
+                var mappedItems = _mapper.Map<List<AnswerDTO>>(items);
+
+                return new BaseResponse<List<AnswerDTO>>
+                    (data: mappedItems, message: Messages.RetrievedSuccessfully, errors: [], true);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<List<AnswerDTO>>
+                    (null, Messages.Error, new List<string> { ex.Message }, false);
+            }
+        }
+    }
+}
